91bdc8382a81d2dcb4d4fadb39f39a01d3acdbce
choijungwoo493 <jwoo1016@gmail.com>
2023-03-30 09:07:34 +0900

    ADD files to w4d4

---
+++ src/week4/day3/sfefef.java
@@ -0,0 +1,13 @@
+//package week4.day3;
+//
+//import java.util.stream.Stream;
+//
+//public class sfefef {
+//
+//    public static void main(String[] args) {
+//        int num = 1;
+//        Stream.generate(() -> num + 1).map(num -> num + 1).forEach(System.out::println);
+//    }
+//
+//    public void sfsd(){}
+//}
